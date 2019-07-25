e86d56ff69bc177e9f1f361e6ece8c969021f2ec
rikalica <hatajimui@yahoo.co.jp>
2019-07-25 22:48:36 +0900

    cursor.moveToNext()メソッド追加

--- app/src/main/java/com/websarva/wings/android/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/autoslideshowapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.websarva.wings.android.autoslideshowapp
 import android.Manifest
 import android.content.ContentUris
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     private val PERMISSIONS_REQUEST_CODE = 100
 
+    var cursor: Cursor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,7 +29,8 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
                 // 許可されている
                 getContentsInfo()
             } else {
-                // 許可されていないので許可ダイアログを表示する
+
+
                 requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
             }
             // Android 5系以下の場合
@@ -34,9 +38,8 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
             getContentsInfo()
         }
 
-
         val resolver = contentResolver
-        val cursor = resolver.query(
+        cursor = resolver.query(
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
             null, // 項目(null = 全項目)
             null, // フィルタ条件(null = フィルタなし)
@@ -49,17 +52,18 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         play_button.setOnClickListener(this)
     }
 
+
     override fun onClick(v: View) {
         if (v.id == R.id.next_button) {
             Log.d("UI_PARTS", "進むボタンをタップしました")
 
-            cursor.moveToNext()
-            //if (cursor.moveToFirst()) {
-            //    val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-            //    val id = cursor.getLong(fieldIndex)
-            //    val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-            //    cursor.moveToNext()
-            //}
+            cursor!!.moveToNext()
+            if (cursor!!.moveToFirst()) {
+                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor!!.getLong(fieldIndex)
+                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                cursor!!.moveToNext()
+            }
         } else if (v.id == R.id.back_button) {
             Log.d("UI_PARTS", "戻るボタンをタップしました")
         } else if (v.id == R.id.play_button) {
