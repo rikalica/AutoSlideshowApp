0455a138da17e3e9e957afbf84f6a4f365609e52
rikalica <hatajimui@yahoo.co.jp>
2019-07-31 10:15:58 +0900

    getImageメソッド追加

--- app/src/main/java/com/websarva/wings/android/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/autoslideshowapp/MainActivity.kt
@@ -38,92 +38,65 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
             getContentsInfo()
         }
 
-        val resolver = contentResolver
-        cursor = resolver.query(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-            null, // 項目(null = 全項目)
-            null, // フィルタ条件(null = フィルタなし)
-            null, // フィルタ用パラメータ
-            null // ソート (null ソートなし)
-        )
-        if(cursor!!.moveToFirst()){
-            next_button.setOnClickListener(this)
-            back_button.setOnClickListener(this)
-            play_button.setOnClickListener(this)
-        }
+        //val resolver = contentResolver
+        //cursor = resolver.query(
+        //    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+        //    null, // 項目(null = 全項目)
+        //    null, // フィルタ条件(null = フィルタなし)
+        //    null, // フィルタ用パラメータ
+        //    null // ソート (null ソートなし)
+        //)
+        //if(cursor!!.moveToFirst()){
+            //next_button.setOnClickListener(this)
+            //back_button.setOnClickListener(this)
+            //play_button.setOnClickListener(this)
+        //}
     }
 
 
     override fun onClick(v: View) {
         if (v.id == R.id.next_button) {
-            if(mTimer == null) {
-                Log.d("UI_PARTS", "進むボタンをタップしました")
+            Log.d("UI_PARTS", "進むボタンをタップしました")
 
-                if (cursor!!.moveToNext()) {
-
-                    val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
-                    val id = cursor!!.getLong(fieldIndex)
-                    val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-                    Log.d("ANDROID", "URI : " + imageUri.toString())
-                    imageView.setImageURI(imageUri)
-                } else {
-                    cursor!!.moveToFirst()
-                    val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
-                    val id = cursor!!.getLong(fieldIndex)
-                    val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-                    imageView.setImageURI(imageUri)
-                }
+            if (cursor!!.moveToNext()) {
+                getImage()
+            } else {
+                cursor!!.moveToFirst()
+                getImage()
             }
         } else if (v.id == R.id.back_button) {
-            if(mTimer == null) {
-                Log.d("UI_PARTS", "戻るボタンをタップしました")
+            Log.d("UI_PARTS", "戻るボタンをタップしました")
 
-                if (cursor!!.moveToPrevious()) {
-                    val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
-                    val id = cursor!!.getLong(fieldIndex)
-                    val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-                    imageView.setImageURI(imageUri)
-                    Log.d("ANDROID", "URI : " + imageUri.toString())
-
-                } else {
-                    cursor!!.moveToLast()
-                    val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
-                    val id = cursor!!.getLong(fieldIndex)
-                    val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-                    imageView.setImageURI(imageUri)
-                }
+            if (cursor!!.moveToPrevious()) {
+                getImage()
+            } else {
+                cursor!!.moveToLast()
+                getImage()
             }
         } else if (v.id == R.id.play_button) {
             Log.d("UI_PARTS", "再生/停止ボタンをタップしました")
             if(mTimer == null) {
                 play_button.text = "停止"
+                next_button.setEnabled(false)
+                back_button.setEnabled(false)
                 mTimer = Timer()
                 mTimer!!.schedule(object : TimerTask() {
                     override fun run() {
                         mHandler.post {
                             Log.d("UI_PARTS", "IF文")
                             if (cursor!!.moveToNext()) {
-                                // indexからIDを取得し、そのIDから画像のURIを取得する
-                                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
-                                val id = cursor!!.getLong(fieldIndex)
-                                val imageUri =
-                                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-                                imageView.setImageURI(imageUri)
-                                Log.d("ANDROID", "URI : " + imageUri.toString())
+                                getImage()
                             } else {
                                 cursor!!.moveToFirst()
-                                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
-                                val id = cursor!!.getLong(fieldIndex)
-                                val imageUri =
-                                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-                                imageView.setImageURI(imageUri)
-                                Log.d("ANDROID", "URI : " + imageUri.toString())
+                                getImage()
                             }
                         }
                     }
                 }, 2000, 2000)
             } else {
                 play_button.text = "再生"
+                next_button.setEnabled(true)
+                back_button.setEnabled(true)
                 mTimer!!.cancel()
                 mTimer = null
             }
@@ -145,7 +118,7 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
     private fun getContentsInfo() {
         // 画像の情報を取得する
         val resolver = contentResolver
-        val cursor = resolver.query(
+        cursor = resolver.query(
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
             null, // 項目(null = 全項目)
             null, // フィルタ条件(null = フィルタなし)
@@ -153,14 +126,20 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
             null // ソート (null ソートなし)
         )
 
-        if (cursor.moveToFirst()) {
-            // indexからIDを取得し、そのIDから画像のURIを取得する
-            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-            val id = cursor.getLong(fieldIndex)
-            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-
-            imageView.setImageURI(imageUri)
+        if (null == cursor && cursor!!.getCount() < 1) {
+            if (cursor!!.moveToFirst()) {
+                next_button.setOnClickListener(this)
+                back_button.setOnClickListener(this)
+                play_button.setOnClickListener(this)
+            }
         }
-        cursor.close()
+    }
+
+    private fun  getImage() {
+        val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+        val id = cursor!!.getLong(fieldIndex)
+        val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+        Log.d("ANDROID", "URI : " + imageUri.toString())
+        imageView.setImageURI(imageUri)
     }
 }
